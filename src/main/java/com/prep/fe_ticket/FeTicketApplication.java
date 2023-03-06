f567f4e81f43d82303e7345586fec1a04f3f1fc0
Ray-Pawin <88267743+Ray-Pawin@users.noreply.github.com>
2023-03-06 13:40:06 +0700

    first init

---
+++ src/main/java/com/prep/fe_ticket/FeTicketApplication.java
@@ -0,0 +1,13 @@
+package com.prep.fe_ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeTicketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prep/fe_ticket/FeTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prep.fe_ticket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeTicketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
